9411c9b7b03b5045f4171c59986e166c328ac170
Inna <inna.tereshchenko1@gmail.com>
2020-02-24 13:32:07 +0200

    Updates

---
+++ precache-manifest.583e331adcf582d1df34360b4b63b5a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c04d40a60db469f8b2c16b9d50c997",
+    "url": "/react-app_test-task/index.html"
+  },
+  {
+    "revision": "45067bdc4f3fc14d4b77",
+    "url": "/react-app_test-task/static/css/main.3abd49de.chunk.css"
+  },
+  {
+    "revision": "c3416610fcc0d846cc0a",
+    "url": "/react-app_test-task/static/js/2.6dd6d483.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-app_test-task/static/js/2.6dd6d483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45067bdc4f3fc14d4b77",
+    "url": "/react-app_test-task/static/js/main.9983d1eb.chunk.js"
+  },
+  {
+    "revision": "1531f78dfae8554241f1",
+    "url": "/react-app_test-task/static/js/runtime-main.5872f4bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app_test-task/static/media/logo.5d5d9eef.svg"
+  }
+]);
